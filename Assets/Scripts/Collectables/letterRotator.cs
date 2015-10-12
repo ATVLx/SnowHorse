3f68195de345922e90c3dc3f1367638c69f85a2d
Chris Figueroa <chris@kinifigames.com>
2015-10-12 14:41:36 -0700

    Added Little Tokyo Level, Collectable HORSE Letters

---
+++ Assets/Scripts/Collectables/letterRotator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class letterRotator : MonoBehaviour {
+
+	private float rotationSpeed = 80.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed, Space.World);
+	
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		Debug.Log("Collected Letter");
+		Destroy(this.gameObject);
+	}
+
+}
