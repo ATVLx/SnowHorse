816ee17399230ade84429c849cd4587a311e3420
kinifi <chrisfigueroa16@gmail.com>
2015-10-14 11:10:18 -0700

    Added Title Screen

---
+++ Assets/Scripts/Camera/rotateAroundObject.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotateAroundObject : MonoBehaviour {
+
+    public Transform objectToRotateAround;
+    public Vector3 cameraOffSet;
+    public float rotatingSpeed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.RotateAround(objectToRotateAround.position, cameraOffSet, rotatingSpeed * Time.deltaTime);
+
+	}
+}
