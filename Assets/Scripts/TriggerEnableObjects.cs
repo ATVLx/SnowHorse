07d95720ecd90618b776f0da03ac5dcedd9f0527
kinifi <chrisfigueroa16@gmail.com>
2015-11-04 13:38:42 -0800

    merged bigair and armin's level

--- Assets/Scripts/TriggerEnableObjects.cs
+++ Assets/Scripts/TriggerEnableObjects.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class TriggerEnableObjects : MonoBehaviour {
 
     public GameObject[] m_GameObjectsToEnable;
+    [Range(0.5f, 10.0f)]
+    public float minRange = 0.5f;
+    [Range(0.5f, 10.0f)]
+    public float maxRange = 1.8f;
 
 	// Use this for initialization
 	void Start () {
@@ -17,7 +21,7 @@ public class TriggerEnableObjects : MonoBehaviour {
         for (int i = 0; i < m_GameObjectsToEnable.Length; i++)
         {
             m_GameObjectsToEnable[i].SetActive(true);
-            Destroy(m_GameObjectsToEnable[i], Random.Range(0.5f, 1.8f));
+            Destroy(m_GameObjectsToEnable[i], Random.Range(minRange, maxRange));
         }
     }
 }
