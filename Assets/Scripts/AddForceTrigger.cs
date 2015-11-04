27884a483d38912874324c79da531bab769fb683
kinifi <chrisfigueroa16@gmail.com>
2015-11-03 16:52:39 -0800

    added rigidbody up push

---
+++ Assets/Scripts/AddForceTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddForceTrigger : MonoBehaviour {
+
+    public float forceToAdd = 300.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        this.GetComponent<BoxCollider>().isTrigger = true;
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Board")
+        {
+            other.GetComponent<Rigidbody>().AddForce(Vector3.up * forceToAdd);
+        }
+    }
+}
