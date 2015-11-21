44fdb28fb1f7e4922f3fb01b0bee8f39953b202c
kinifi <chrisfigueroa16@gmail.com>
2015-11-20 16:25:37 -0800

    Added JSON serialization

---
+++ Assets/Scripts/Customization/HorseCustomization.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+using System.Collections;
+
+[Serializable]
+public class HorseCustomization : MonoBehaviour {
+
+    public int mBoard = 0;
+    public int mHat = 0;
+    private string jsonData;
+
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+
+    public void doesCustomizationExist()
+    {
+        if (File.Exists(Application.dataPath + "/horseCustomization.json"))
+        {
+            //load the file
+            JsonUtility.FromJson<HorseCustomization>(Application.dataPath + "/horseCustomization.json");
+            Debug.Log("Loaded: " + jsonData);
+        }
+        else
+        {
+            //create the file
+            File.Create(Application.dataPath + "/horseCustomization.json");
+        }
+        
+    }
+
+    public void setCustomizationToFile()
+    {
+        jsonData = JsonUtility.ToJson(this, true);
+        File.WriteAllText(Application.dataPath + "/horseCustomization.json", jsonData);
+
+        Debug.Log("Saved Data: " + jsonData);
+    }
+
+    public void setHat(int hat)
+    {
+        //set the hat now
+        mHat = hat;
+        setCustomizationToFile();
+    }
+
+    public void setBoard(int board)
+    {
+        //set the board now
+        mBoard = board;
+        setCustomizationToFile();
+    }
+
+}
