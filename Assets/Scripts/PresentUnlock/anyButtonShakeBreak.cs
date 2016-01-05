9606cfe856be0bcfcc412900e37b4c266bd360c3
Chris Figueroa <chris@kinifigames.com>
2016-01-04 23:00:41 -0800

    added presents

---
+++ Assets/Scripts/PresentUnlock/anyButtonShakeBreak.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class anyButtonShakeBreak : MonoBehaviour {
+
+	private bool hasActivated = false;
+	public GameObject actionText, confetti;
+
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.anyKeyDown && hasActivated == false)
+		{
+			hasActivated = true;
+			actionText.SetActive(false);
+			confetti.SetActive(true);
+			iTween.ShakePosition(this.gameObject, new Vector3(0.3f, 0.3f, 0.3f), 0.5f);
+			Invoke("destroyPackage", 0.5f);
+		}
+	
+	}
+
+	private void destroyPackage()
+	{
+		Destroy(this.gameObject);
+	}
+}
---
+++ Assets/Scripts/PresentUnlock/bgMaterialView.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bgMaterialView : MonoBehaviour {
+
+	private float scrollSpeed = 0.01f;
+	private Renderer rend;
+
+	void Start() {
+		
+		rend = GetComponent<Renderer>();
+	}
+
+	void Update() {
+		
+		float offset = Time.time * scrollSpeed;
+		rend.material.mainTextureOffset = new Vector2(offset, offset);
+	
+	}
+}
